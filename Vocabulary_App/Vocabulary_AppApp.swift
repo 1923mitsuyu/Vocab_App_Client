07a6f097cd5e73b7b1a1be97da58bba9a6c2fe42
Ikufumi <Ikufumi665@gmail.com>
2024-11-05 20:20:00 +1000

    Initial Commit

---
+++ Vocabulary_App/Vocabulary_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vocabulary_AppApp.swift
+//  Vocabulary_App
+//
+//  Created by Ikufumi Mitsuyu on 5/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Vocabulary_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Vocabulary_AppTests/Vocabulary_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Vocabulary_AppTests.swift
+//  Vocabulary_AppTests
+//
+//  Created by Ikufumi Mitsuyu on 5/11/2024.
+//
+
+import Testing
+
+struct Vocabulary_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
