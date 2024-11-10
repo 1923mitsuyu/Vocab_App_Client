ae532d0b5d13bf0d16d3994a28fc56992207aae1
Ikufumi Mitsuyu <Ikufumi665@gmail.com>
2024-11-10 19:55:31 +1000

    Removed some codes

--- Vocabulary_App/ViewModels/DeckViewModel.swift
+++ Vocabulary_App/ViewModels/DeckViewModel.swift
@@ -4,20 +4,7 @@ class DeckViewModel : ObservableObject {
     
     @Published var decks: [Deck] = sampleDecks
     @Published var emptyDecks: [Deck] = []
-    
-    func fetchDecks () {
-        updateDecksOrder()
-        objectWillChange.send()
-    }
-    
-    func updateDecksOrder () {
-        for (index, _) in decks.enumerated() {
-            decks[index].listOrder = index
-        }
-        // Notify SwiftUI of changes (not necessary unless needed explicitly)
-        objectWillChange.send()
-    }
-    
+        
     func checkIfNameExists (_ name: String) -> Bool {
         for deck in decks {
             if deck.name == name {
