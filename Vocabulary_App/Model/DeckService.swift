7f5b8f952744ddba067fe3e94bfa74222e09e9ce
Ikufumi Mitsuyu <Ikufumi665@gmail.com>
2024-11-14 00:47:00 +1000

    Comment out all the codes

--- Vocabulary_App/Model/DeckService.swift
+++ Vocabulary_App/Model/DeckService.swift
@@ -1,42 +1,44 @@
-import Foundation
-
-class DeckService {
- 
-    // Deckの取得
-    func fetchDeck(completion: @escaping (Result<[Deck], Error>) -> Void) {
-        let endpoint = "https://api.example.com/deck"
-        
-        NetworkManager.shared.request(endpoint: endpoint, method: .get) { (result: Result<[Deck], Error>) in
-            completion(result)
-        }
-    }
-    
-    // Deckの追加
-    func createDeck(name: String, description: String?, completion: @escaping (Result<Deck, Error>) -> Void) {
-        let endpoint = "https://api.example.com/deck"
-        let parameters: [String: Any] = ["name": name, "description": description ?? ""]
-        
-        NetworkManager.shared.request(endpoint: endpoint, method: .post, parameters: parameters) { (result: Result<Deck, Error>) in
-            completion(result)
-        }
-    }
-    
-    // Deckの編集
-    func editDeck(deckId: Int, newName: String, completion: @escaping (Result<Deck, Error>) -> Void) {
-        let endpoint = "https://api.example.com/deck/\(deckId)"
-        let parameters: [String: Any] = ["name": newName]
-        
-        NetworkManager.shared.request(endpoint: endpoint, method: .put, parameters: parameters) { (result: Result<Deck, Error>) in
-            completion(result)
-        }
-    }
-    
-    // Deckの削除
-    func deleteDeck(deckId: Int, completion: @escaping (Result<Void, Error>) -> Void) {
-        let endpoint = "https://api.example.com/deck/\(deckId)"
-        
-        NetworkManager.shared.request(endpoint: endpoint, method: .delete) { (result: Result<Void, Error>) in
-            completion(result)
-        }
-    }
-}
+//import Foundation
+//
+//class DeckService {
+// 
+//    static let shared = DeckService()
+//    private init() {} 
+//    // Deckの取得
+//    func fetchDeck(completion: @escaping (Result<[Deck], Error>) -> Void) {
+//        let endpoint = "https://api.example.com/deck"
+//        
+//        NetworkManager.shared.request(endpoint: endpoint, method: .get) { (result: Result<[Deck], Error>) in
+//            completion(result)
+//        }
+//    }
+//    
+//    // Deckの追加
+//    func createDeck(name: String, description: String?, completion: @escaping (Result<Deck, Error>) -> Void) {
+//        let endpoint = "https://api.example.com/deck"
+//        let parameters: [String: Any] = ["name": name, "description": description ?? ""]
+//        
+//        NetworkManager.shared.request(endpoint: endpoint, method: .post, parameters: parameters) { (result: Result<Deck, Error>) in
+//            completion(result)
+//        }
+//    }
+//    
+//    // Deckの編集
+//    func editDeck(deckId: Int, newName: String, completion: @escaping (Result<Deck, Error>) -> Void) {
+//        let endpoint = "https://api.example.com/deck/\(deckId)"
+//        let parameters: [String: Any] = ["name": newName]
+//        
+//        NetworkManager.shared.request(endpoint: endpoint, method: .put, parameters: parameters) { (result: Result<Deck, Error>) in
+//            completion(result)
+//        }
+//    }
+//    
+//    // Deckの削除
+//    func deleteDeck(deckId: Int, completion: @escaping (Result<Void, Error>) -> Void) {
+//        let endpoint = "https://api.example.com/deck/\(deckId)"
+//        
+//        NetworkManager.shared.request(endpoint: endpoint, method: .delete) { (result: Result<Void, Error>) in
+//            completion(result)
+//        }
+//    }
+//}
--- Vocabulary_App/Model/NetworkManager.swift
+++ Vocabulary_App/Model/NetworkManager.swift
@@ -1,82 +1,82 @@
-import Foundation
-
-enum HTTPMethod: String {
-    case get = "GET"
-    case post = "POST"
-    case put = "PUT"
-    case delete = "DELETE"
-}
-
-enum NetworkError: Error {
-    case invalidURL
-    case emptyData
-    case decodingError
-    case unexpectedData
-    case other(Error)
-}
-
-// Manage a common process for any http requests
-class NetworkManager {
-    
-    static let shared = NetworkManager()
-    
-    private init () {}
-    
-    func request<T: Decodable>(endpoint: String, method: HTTPMethod, parameters: [String: Any]? = nil, completion: @escaping (Result<T, Error>) -> Void) {
-        
-        guard let url = URL(string: endpoint) else {
-            completion(.failure(NetworkError.invalidURL))  
-            return
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = method.rawValue
-        
-        if let parameters = parameters {
-            request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
-        }
-        
-        URLSession.shared.dataTask(with: request) { data, request, error in
-            if let error {
-                completion(.failure(NetworkError.other(error)))
-                return
-            }
-            
-            guard let data else {
-                completion(.failure(NetworkError.emptyData))
-                return
-            }
-        
-            do {
-                let decoder = JSONDecoder()
-                let response = try decoder.decode(T.self, from: data)
-                completion(.success(response))
-            } catch {
-                completion(.failure(NetworkError.decodingError))
-            }
-        }.resume()
-    }
-    
-    func request(endpoint: String, method: HTTPMethod, parameters: [String: Any]? = nil, completion: @escaping (Result<Void, Error>) -> Void) {
-        
-        var request = URLRequest(url: URL(string: endpoint)!)
-        request.httpMethod = method.rawValue
-        if let parameters {
-            request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
-        }
-        
-        URLSession.shared.dataTask(with: request) { data, request, error in
-            if let error {
-                completion(.failure(error))
-                return
-            }
-            
-            guard data != nil else {
-                completion(.success(()))
-                return
-            }
-            
-            completion(.failure(NetworkError.unexpectedData))
-        }.resume()
-    }
-}
+//import Foundation
+//
+//enum HTTPMethod: String {
+//    case get = "GET"
+//    case post = "POST"
+//    case put = "PUT"
+//    case delete = "DELETE"
+//}
+//
+////enum NetworkError: Error {
+////    case invalidURL
+////    case emptyData
+////    case decodingError
+////    case unexpectedData
+////    case other(Error)
+////}
+//
+//// Manage a common process for any http requests
+//class NetworkManager {
+//    
+//    static let shared = NetworkManager()
+//    
+//    private init () {}
+//    
+//    func request<T: Decodable>(endpoint: String, method: HTTPMethod, parameters: [String: Any]? = nil, completion: @escaping (Result<T, Error>) -> Void) {
+//        
+//        guard let url = URL(string: endpoint) else {
+//            completion(.failure(NetworkError.invalidURL))  
+//            return
+//        }
+//        
+//        var request = URLRequest(url: url)
+//        request.httpMethod = method.rawValue
+//        
+//        if let parameters = parameters {
+//            request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+//        }
+//        
+//        URLSession.shared.dataTask(with: request) { data, request, error in
+//            if let error {
+//                completion(.failure(NetworkError.other(error)))
+//                return
+//            }
+//            
+//            guard let data else {
+//                completion(.failure(NetworkError.emptyData))
+//                return
+//            }
+//        
+//            do {
+//                let decoder = JSONDecoder()
+//                let response = try decoder.decode(T.self, from: data)
+//                completion(.success(response))
+//            } catch {
+//                completion(.failure(NetworkError.decodingError))
+//            }
+//        }.resume()
+//    }
+//    
+//    func request(endpoint: String, method: HTTPMethod, parameters: [String: Any]? = nil, completion: @escaping (Result<Void, Error>) -> Void) {
+//        
+//        var request = URLRequest(url: URL(string: endpoint)!)
+//        request.httpMethod = method.rawValue
+//        if let parameters {
+//            request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+//        }
+//        
+//        URLSession.shared.dataTask(with: request) { data, request, error in
+//            if let error {
+//                completion(.failure(error))
+//                return
+//            }
+//            
+//            guard data != nil else {
+//                completion(.success(()))
+//                return
+//            }
+//            
+//            completion(.failure(NetworkError.unexpectedData))
+//        }.resume()
+//    }
+//}
