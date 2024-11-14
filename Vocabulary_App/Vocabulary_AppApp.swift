e4a47625ad24c70aa8f59603ff4b16c509e6b4a9
Ikufumi Mitsuyu <Ikufumi665@gmail.com>
2024-11-14 22:50:44 +1000

    Modify the codes to test login and sign up

--- Vocabulary_App/Vocabulary_AppApp.swift
+++ Vocabulary_App/Vocabulary_AppApp.swift
@@ -3,11 +3,10 @@ import SwiftUI
 @main
 struct Vocabulary_AppApp: App {
     
-//    var network = Network()
     var body: some Scene {
         WindowGroup {
-            MainView()
-//                .environmentObject(network)
+           MainView()
+            // Login()
         }
     }
 }
