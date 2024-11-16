f951f7019eec01804e56d8f00e9c8e874f85af03
Ikufumi Mitsuyu <Ikufumi665@gmail.com>
2024-11-16 21:01:08 +1000

    Implemented UI for the setting view

--- Vocabulary_App/View/Setting/SettingView.swift
+++ Vocabulary_App/View/Setting/SettingView.swift
@@ -4,57 +4,55 @@ struct SettingView: View {
     
     @State private var reminderIsOn = false
     @State private var isHovering = false
+    @State private var selectedColor: Color = .blue
+    
+    let colors: [Color] = [.red, .green, .blue, .yellow, .purple, .orange]
     
     var body: some View {
         VStack {
             
-            Text("Notification")
+            Text("Setting")
                 .font(.system(size: 27, weight: .semibold, design: .rounded))
                 .padding(.top, 20)
                 .padding(.bottom, -3)
             
-            HStack {
-                Image(systemName: "questionmark.app.fill")
-                    .font(.system(size: 15))
-                    .foregroundColor(.white)
-                    .padding(.trailing,5)
-                    .onTapGesture {
-                        isHovering.toggle()
-                    }
-                    .overlay {
-                        if isHovering {
-                            Text("You will receive regular notifications to remind you to study!")
-                                .padding(10)
-                                .background(Color.black.opacity(0.75))
-                                .foregroundColor(.white)
-                                .cornerRadius(8)
-                                .transition(.opacity)
-                                .frame(width: 250)
-                                .lineLimit(nil)
-                                .multilineTextAlignment(.leading)
-                                .fixedSize(horizontal: false, vertical: true)
-                                .padding(.leading, 100)
-                                .padding(.top,200)
-                                .offset(y: -30)
+            List {
+                Section(header: Text("Background Color").font(.headline)){
+                    HStack {
+                        ForEach(colors, id: \.self) { color in
+                            Circle()
+                                .fill(color)
+                                .frame(width: 40, height: 40)
+                                .onTapGesture {
+                                    selectedColor = color
+                                }
+                                .overlay(Circle().stroke(.white, lineWidth: 2))
+                                .padding(5)
                         }
                     }
+                }
+                
+                Section(header: Text("Reminder").font(.headline)){
+                    HStack {
+                        Text("Notification")
+                            .font(.system(size: 23, weight: .semibold, design: .rounded))
+                            .frame(width: 200)
+                            .padding(.vertical,10)
+                            .padding(.trailing,90)
             
-                Text("Recieve notification")
-                    .padding(.top, 30)
-                    .padding(.bottom,10)
-                    .font(.system(size: 20, weight: .semibold, design: .rounded))
-                
-                Toggle("", isOn: $reminderIsOn)
-                    .labelsHidden()
-                    .padding(.top, 20)
-                    .padding(.leading,10)
-                
+                        Toggle("", isOn: $reminderIsOn)
+                            .labelsHidden()
+                            .padding(.leading,10)
+                            .padding(.trailing,50)
+                        
+                    }
+                }
             }
-            Spacer()
+            .scrollContentBackground(.hidden)
         }
         .frame(maxWidth:.infinity, maxHeight:.infinity)
-        .background(.blue.gradient)
-    
+        .background(selectedColor)
+        .animation(.easeInOut, value: selectedColor)
     }
 }
 
