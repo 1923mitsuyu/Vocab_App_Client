92a6506c40086569b5a4b6f55b3fd0115afb62ab
Ikufumi Mitsuyu <Ikufumi665@gmail.com>
2024-11-24 19:53:15 +1000

    Fixed the issue of tab view disappearing after saving a new deck because of incorrect use of the main tab.

---
+++ Vocabulary_App/View/StudyView/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  Vocabulary_App
+//
+//  Created by Ikufumi Mitsuyu on 24/11/2024.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test()
+}
